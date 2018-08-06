361c7a6a9935fac7ae97fe3e70fa1b480e6acb76
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2018-08-06 15:19:54 -0500

    feat: Create form with button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import TodoList from "./TodoList";
 
 const destination = document.querySelector("#container");
 
 ReactDOM.render(
   <div>
-      <h2>To-Do List</h2>
+      <TodoList/>
   </div>,
   destination
 );
