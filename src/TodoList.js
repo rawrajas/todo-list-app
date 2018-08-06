b75d3737a0c14674354ee6c4264bbcdbff399665
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2018-08-06 15:27:25 -0500

    feat: Add constructor function with array

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items:[]
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e){
+        
+    }
+
     render() {
         return (
           <div className="todoListMain">
               <div className="header">
-                  <form>
-                      <input placeholder="Enter task">
+                  <form onSubmit={this.addItem}>
+                      <input ref={(a) => this._inputElement = a}
+                          placeholder="Enter task">
                       </input>
                       <button type="submit">Add</button>
                   </form>
