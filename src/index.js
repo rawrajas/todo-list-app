d1fafb2dd443063e293b1edb1e20dece8689fc93
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2018-08-06 14:40:33 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const destination = document.querySelector("#container");
+
+ReactDOM.render(
+  <div>
+      <h2>To-Do List</h2>
+  </div>,
+  destination
+);
